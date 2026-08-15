a555717ad54864c45ed22d5ff8e147236e7dc8af
quame3438-bit <quame3438@gmail.com>
2026-08-15 15:08:06 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,2 +1,7 @@
-self.addEventListener('install',()=>self.skipWaiting());
-self.addEventListener('activate',()=>self.clients.claim());
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', () => {
+  self.clients.claim();
+});
